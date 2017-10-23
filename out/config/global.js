3bc735ef8fe1923e1de8421dc68abc4f925e5d5f
Cristian Muñoz <cristian9016@gmail.com>
2017-10-23 17:34:38 -0500

    global remote

--- out/config/global.js
+++ out/config/global.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = {
     secret: "Ganko2017",
-    //  database: {
-    //      host: 'us-cdbr-iron-east-05.cleardb.net',
-    //      user: 'bb942c918452fa',
-    //      password: 'e45e8c49',
-    //      database: 'heroku_6de81001f978b1a'
-    // }
+    //   database: {
+    //       host: 'us-cdbr-iron-east-05.cleardb.net',
+    //       user: 'bb942c918452fa',
+    //       password: 'e45e8c49',
+    //       database: 'heroku_6de81001f978b1a'
+    //  }
     database: {
         host: 'localhost',
         user: 'root',
