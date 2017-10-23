c5f077a32734a0311e411f77e043d719d61081c1
Cristian Muñoz <cristian9016@gmail.com>
2017-10-23 17:06:01 -0500

    se cambia global config para remoto

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,16 +1,16 @@
 
 export const config = {
     secret : "Ganko2017",
-    // database: {
-    //     host: 'us-cdbr-iron-east-05.cleardb.net',
-    //     user: 'bb942c918452fa',
-    //     password: 'e45e8c49',
-    //     database: 'heroku_6de81001f978b1a'
-    // }
      database: {
-         host: 'localhost',
-         user: 'root',
-         password: '12345',
-         database: 'prueba1'
+         host: 'us-cdbr-iron-east-05.cleardb.net',
+         user: 'bb942c918452fa',
+         password: 'e45e8c49',
+         database: 'heroku_6de81001f978b1a'
     }
+    //  database: {
+    //      host: 'localhost',
+    //      user: 'root',
+    //      password: '12345',
+    //      database: 'prueba1'
+    // }
 }
