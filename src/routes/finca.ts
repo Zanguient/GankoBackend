7bafeeacaccba1ea96f4b511639c8179a7aaabde
Cristian Muñoz <cristian9016@gmail.com>
2017-10-25 09:40:32 -0500

    delete-finca implementado

--- src/routes/finca.ts
+++ src/routes/finca.ts
@@ -6,6 +6,6 @@ const finca: Router = Router();
 
 finca.get('/get-fincas',ValidateToken,getFinca);
 finca.post('/add-finca',ValidateToken,addFinca);
-finca.delete('/delete-finca',ValidateToken,deleteFinca);
+finca.delete('/delete-finca/:idfinca',ValidateToken,deleteFinca);
 
 export default finca;
