646b99f1cf00ff6701d8a5b80a5531f043c2faa4
Cristian Muñoz <cristian9016@gmail.com>
2018-05-15 21:33:39 -0500

    routes in app.ts updated

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,16 @@ import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import user from './routes/user';
 import bovino from './routes/bovino';
+import alimentacion from './routes/alimentacion';
+import ceba from './routes/ceba';
+import finca from './routes/finca';
+import leche from './routes/leche';
+import manejo from './routes/manejo';
+import pajilla from './routes/pajilla';
+import produccion from './routes/produccion';
+import sanidad from './routes/sanidad';
+import vacuna from './routes/vacuna';
+import venta from './routes/venta';
 import cookieParser = require('cookie-parser'); // this module doesn't use the ES6 default export yet
 import cors = require('cors');
 
@@ -20,8 +30,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/user', user);
-app.use('/bovino', bovino);
+app.use('/api/v1/user', user);
+app.use('/api/v1/bovino', bovino);
+app.use('/api/v1/alimentacion', alimentacion);
+app.use('/api/v1/ceba', ceba);
+app.use('/api/v1/finca', finca);
+app.use('/api/v1/leche', leche);
+app.use('/api/v1/manejo', manejo);
+app.use('/api/v1/pajilla', pajilla);
+app.use('/api/v1/produccion', produccion);
+app.use('/api/v1/sanidad', sanidad);
+app.use('/api/v1/vacuna', vacuna);
+app.use('/api/v1/venta', venta);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
