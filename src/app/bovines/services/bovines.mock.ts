320bbb8e9f1d1c1d27c015b4c3831eb4c9112ea9
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-07-25 18:10:57 -0500

    Bovine service implemented

---
+++ src/app/bovines/services/bovines.mock.ts
@@ -0,0 +1,40 @@
+import { Bovino } from '../../shared/models/bovine.model';
+
+export function bovines(): Bovino[] {
+    return [
+        makeBovine(1),
+        makeBovine(2),
+        makeBovine(3),
+        makeBovine(4)
+    ];
+}
+
+export function bovine(): Bovino {
+    return makeBovine(1);
+}
+
+function makeBovine(index: number): Bovino {
+    return {
+        id: '123',
+        nombre: 'Lupita ' + index,
+        type: 'Bovino',
+        tipo: 'Leche',
+        codigo: '000' + index,
+        fechaIngreso: new Date(),
+        genero: 'Hembra',
+        proposito: 'Leche',
+        peso: 100,
+        color: 'cafe',
+        raza: 'Runa',
+        lote: 1,
+        partos: 0,
+        precioCompra: 30000,
+        procedencia: 'Compra',
+
+        retirado: false,
+        finca: '123',
+        destete: true,
+        fechaDestete: new Date(),
+        celos: []
+    };
+}
--- src/app/bovines/services/bovines.service.ts
+++ src/app/bovines/services/bovines.service.ts
@@ -1,7 +1,59 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SessionService } from '../../core/services/session.service';
+import { Bovino } from '../../shared/models/bovine.model';
+import { BaseService } from '../../util/base-service';
+import { Observable, timer } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Rspn } from '../../shared/models/response.model';
+import { validate } from '../../util/http-util';
+import { bovines, bovine } from './bovines.mock';
 
 @Injectable()
-export class BovinesService {
+export class BovinesService extends BaseService<Bovino> {
 
-  constructor() { }
+  data: Bovino[] = [];
+
+  constructor(private http: HttpClient, private session: SessionService) {
+    super();
+  }
+
+  add(item: Bovino): Observable<string> {
+    item.finca = this.session.farmId;
+    return timer(500).pipe(
+      map(() => new Rspn(true, '')), // simular respuesta
+      map(x => validate(x)),
+      tap(() => this.data.push(item))
+    );
+  }
+
+  list(...params: any[]): Observable<Bovino[]> {
+    return timer(500).pipe(
+      tap(() => this.data = this.data.length > 0 ? this.data : bovines()),
+      map(() => new Rspn(true, this.data)), // simular respuesta
+      map(x => validate(x))
+    );
+  }
+
+  update(item: Bovino): Observable<string> {
+    return timer(500).pipe(
+      map(() => new Rspn(true, '')), // simular respuesta
+      map(x => validate(x))
+    );
+  }
+
+  remove(id: string): Observable<string> {
+    return timer(500).pipe(
+      map(() => new Rspn(true, '')), // simular respuesta
+      map(x => validate(x))
+    );
+  }
+
+  getById(id: string): Observable<Bovino> {
+    return timer(500).pipe(
+      map(() => new Rspn(true, bovine())),
+      map(x => validate(x))
+    );
+  }
+
 }
