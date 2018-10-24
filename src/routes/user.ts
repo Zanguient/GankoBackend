800095b2ba1c739609cc4662b70637fb2bf2d121
Dario Chamorro <dario.chamorro.v@gmail.com>
2018-10-23 19:23:51 -0500

    remove assets angular

--- src/routes/user.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { login, resetPassword, insert, newPassword } from '../controllers/index';
-import { ValidateToken } from '../middlewares/token-validation';
-
-const user: Router = Router();
-user.post('/login', login);
-user.post('/',insert);
-export default user;
